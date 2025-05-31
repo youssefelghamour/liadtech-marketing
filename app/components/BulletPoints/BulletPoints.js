f8c13faa6376b06c85a9d5cbf71ee1f8aa758fe6
youssef <youssefelghamour@gmail.com>
2025-05-31 16:54:02 +0100

    referencement-site-internet: BulletPoints Component

---
+++ app/components/BulletPoints/BulletPoints.js
@@ -0,0 +1,18 @@
+import { FaCheckCircle } from "react-icons/fa";
+
+
+export default function BulletPoints(props) {
+    return (
+        <div className="flex flex-col items-start justify-center gap-5">
+            {props.items && props.items.map((item, index) => (
+                <div key={index} className="flex items-start gap-4">
+                    <FaCheckCircle className="text-[var(--purple)] min-w-[24px] min-h-[24px]"/>
+                    <div className="text-left">
+                        <p className="font-bold text-lg">{item.title}</p>
+                        <p className="">{item.description}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
--- app/components/SectionSolutions/SectionSolutions.js
+++ app/components/SectionSolutions/SectionSolutions.js
@@ -1,4 +1,4 @@
-import { FaCheckCircle } from "react-icons/fa";
+import BulletPoints from "../BulletPoints/BulletPoints";
 
 
 export default function SectionSolutions(props) {
@@ -28,17 +28,7 @@ export default function SectionSolutions(props) {
                 </div>
 
                 {/* Right */}
-                <div className="flex flex-col items-start justify-center gap-5">
-                    {props.items && props.items.map((item, index) => (
-                        <div key={index} className="flex items-start gap-4">
-                            <FaCheckCircle className="text-[var(--purple)] min-w-[24px] min-h-[24px]"/>
-                            <div className="text-left">
-                                <p className="font-bold text-lg">{item.title}</p>
-                                <p className="">{item.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <BulletPoints {...props}/>
             </div>
         </section>
     );
